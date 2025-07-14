73e76fa7e4c0e46bca634d1eb29ee7584436b152
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2025-07-15 05:21:31 +0530

    Add Google SignIn

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -39,9 +39,8 @@ router.get('/google/callback',
             res.cookie('auth_token', token, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
+                sameSite: 'none',
                 maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-                domain: 'localhost'
             });
 
             // Redirect to frontend
